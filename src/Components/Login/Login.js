7d8d3685c7f31e8b9eb51bd288082dea1a186c7f
md-mahin-islam-mahi <wdmahin.mhm@gmail.com>
2023-02-20 16:35:22 +0600

    designed full login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -9,7 +9,19 @@ const Login = () => {
                     <h2>Login</h2>
                     <div className='field'>
                         <label className='input-text' htmlFor="">Email</label>
-                        <input className='input-field' type="email" />
+                        <input className='input-field' type="email" placeholder='***@***.com' />
+                    </div>
+
+                    <div className='field' id='password-field'>
+                        <label className='input-text' htmlFor="">Password</label>
+                        <input className='input-field' type="password" placeholder='......' />
+                    </div>
+
+                    <input className='login-btn' type="button" value="Login" />
+                    <div className='forgot'>
+                        <p>Forgot password? <span className='click'>
+                            Click Here
+                        </span></p>
                     </div>
                 </div>
             </form>
